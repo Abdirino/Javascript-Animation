1c6a37cc7e742c6875260e8229cc542246614f8a
Abdirino <abdirinoahash@gmail.com>
2023-04-11 11:05:46 +0300

    gsap not working 🤷‍♀️

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 gsap.registerPlugin(Flip);
 
-const links = document.querySelector(".nav-item a");
-const activeNav = document.querySelector(".active-nav");
+const links = document.getElementsByClassName("nav-item a");
+const activeNav = document.getElementsByClassName("active-nav");
 
 if(typeof(links) === "string"){
     links = JSON.parse(links);
 }
 
-links.forEach((link) => {
+links.forEach( link => {
     link.addEventListener("click", () => {
         // turn nav blue
         gsap.to(links, {color : "#565656"});
+
         if(document.activeElement === link){
             gsap.to(links, {color : "#385ae7"});
         }
+
+        // Move the active line
+        const state  = Flip.getState(activeNav);
+        link.appendChild(activeNav);
     });
 });
